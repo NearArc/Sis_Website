b8a6fd57a6d92ab7d517359a8460bf706b175c8a
Max Powell <maxpowell112@yahoo.com>
2025-07-01 19:10:17 -0500

    initial project scaffold

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import "./App.css";
+
+export default function App() {
+  return (
+    <main className="app">
+      <h1>Hello, Sis!</h1>
+      <p>Scroll soon for cool stuff…</p>
+    </main>
+  );
+}
